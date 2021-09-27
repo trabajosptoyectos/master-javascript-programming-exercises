9c8b272f11d94e4c43cc1b20401bea8a198d4174
trabajosptoyectos <ADMISIONAC35@GMAIL.COM>
2021-09-27 11:20:49 +0000

    updt

--- exercises/081-addToFrontOfNew/app.js
+++ exercises/081-addToFrontOfNew/app.js
@@ -1,6 +1,15 @@
 
 function addToFrontOfNew(arr, element) {
     // Write your function here
+    var oldarr = new Array();
+    for (dr in arr) { 
+        console.log(arr[dr]);                   // for each key in the object
+        oldarr.push(arr[dr])                      // then delete the key-value from the object
+      }
+    console.log(oldarr);
+    oldarr.unshift(element)
+  
+    return oldarr; 
 }
 
 var input = [1, 2];
--- exercises/082-addToBackOfNew/app.js
+++ exercises/082-addToBackOfNew/app.js
@@ -1,5 +1,14 @@
 function addToBackOfNew(arr, element) {
     // Write your function here
+    var oldarr = new Array();
+    for (dr in arr) { 
+        console.log(arr[dr]);                   // for each key in the object
+        oldarr.push(arr[dr])                      // then delete the key-value from the object
+      }
+    console.log(oldarr);
+    oldarr.push(element)
+  
+    return oldarr; 
 }
 
 var input = [1, 2];
