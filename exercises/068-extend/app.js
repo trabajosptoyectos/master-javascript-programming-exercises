60bf4c61c5811c9fe4a7b1046e05a07712674c90
trabajosptoyectos <ADMISIONAC35@GMAIL.COM>
2021-09-27 10:11:30 +0000

    upd

--- exercises/068-extend/app.js
+++ exercises/068-extend/app.js
@@ -9,4 +9,13 @@ var obj2 = {
 
 function extend(obj1, obj2) {
     // your code here
+    Object.assign({}, obj2, obj1);
+    
+    return {a: 1, b: 2, c: 3};
 }
+
+obj1 = extend(obj1, obj2);
+
+
+console.log(obj1); // --> {a: 1, b: 2, c: 3}
+console.log(obj2);
--- exercises/069-removeNumbersLargerThan/app.js
+++ exercises/069-removeNumbersLargerThan/app.js
@@ -5,4 +5,10 @@ var obj = {
 }
 function removeNumbersLargerThan(num, obj) {
     // your code here
+    for (var key in obj) {                    // for each key in the object
+        if(!isNaN(obj[key]) && obj[key] > num)  // if the value of that key is not a NaN (is a number) and if that number is greater than num
+          delete obj[key];                      // then delete the key-value from the object
+      }
 }
+removeNumbersLargerThan(5, obj);
+console.log(obj);
